c3a6541202d80e09138e368b6e877b919f026568
cadlitos <111808284+cadlitos@users.noreply.github.com>
2022-08-26 21:51:13 -0500

    Add files via upload

---
+++ script_formula_1.m
@@ -0,0 +1,17 @@
+clear all
+close all
+clc
+
+A = input("ingrese A=");
+B = input("ingrese B=");
+C = input("ingrese C=");
+
+x1=(-B + sqrt(B^2-4*A*C))/(2*A)
+x2=(-B + sqrt(B^2-4*A*C))/(2*A)
+
+disp(["la primera raiz de los datos es ",x1]);
+
+% A=1, B=7, C=3
+
+
+
