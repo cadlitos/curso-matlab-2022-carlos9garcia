ea66372670a9e603ae526f17f51df9b958063ad2
cadlitos <111808284+cadlitos@users.noreply.github.com>
2022-08-27 21:54:41 -0500

    Add files via upload

--- ope_algebraicas.m
+++ ope_algebraicas.m
@@ -1,14 +1,20 @@
 clear all
 close all
 clc
-% 
-syms x 
-% suma = (x^3 + 3*x) + (5*x+2*x^3);
-% pretty (suma)
-% resta = (x^3 + 3*x) - (5*x+2*x^3);
-% pretty (resta)
 
-multiplicacion = expand((3*x+2)*(-2*x+6))
-potencia = expand((x+1)^5)
-% simplify() factoriza
-% 
+syms x
+
+suma = (x^3 + 3*x -12) + (x^3 + 7*x^2 + 8*x +9);
+multiplicacion= expand((x^3 + 3*x -12)*(x^3 + 7*x^2 + 8*x +9));
+
+raices1= roots([1,0,3,-12]);
+raices2= roots([1,7,8,9]);
+
+
+p1=[1,0,3,-12];
+polyval(p1,0);
+
+p2=[1,7,8,9];
+polyval(p2,0);
+
+multi=conv(p1,p2)
